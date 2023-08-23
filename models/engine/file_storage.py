8698bf9a9801b056e633f8af7655b55f30976fff
MohamedH1000 <mohammedhisham115@gmail.com>
2023-08-23 16:34:09 +0000

    MySQL

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,47 +1,69 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""
+Contains the FileStorage class
+"""
+
 import json
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
-    __file_path = 'file.json'
+    """a json file instances to be serielized and back to instances
+     by the deserialization"""
+
+    __file_path = "file.json"
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+    def all(self, cls=None):
+        """objects of the dictionary to be returned"""
+        if cls is not None:
+            n_d = {}
+            for moftah, value in self.__objects.items():
+                if cls == value.__class__ or cls == value.__class__.__name__:
+                    n_d[moftah] = value
+            return n_d
+        return self.__objects
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
-        self.__objects["{}.{}".format(type(obj).__name__, obj.if)] = obj
+        """the object to be set"""
+        if obj is not None:
+            moftah = obj.__class__.__name__ + "." + obj.id
+            self.__objects[moftah] = obj
 
     def save(self):
-        """Saves storage dictionary to file"""
-        odict = {o: self.__objects[o].to_dict() for o in self.__objects.keys()}
-	with open(self.__file_path, "w", encoding="utf-8") as f:
-		json.dumb(odict, f)
+        """objects to the json file to be serialized"""
+        json_objects = {}
+        for moftah in self.__objects:
+            json_objects[moftah] = self.__objects[moftah].to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(json_objects, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+        """the json file to be serialized to oobjects"""
         try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
-        except FileNotFoundError:
+            with open(self.__file_path, 'r') as f:
+                loa = json.load(f)
+            for moftah in loa:
+                self.__objects[moftah] = classes[loa[moftah]["__class__"]](**loa[moftah])
+        except:
             pass
+
+    def delete(self, obj=None):
+        """to delete the objects from the json file"""
+        if obj is not None:
+            moftah = obj.__class__.__name__ + '.' + obj.id
+            if moftah in self.__objects:
+                del self.__objects[moftah]
+
+    def close(self):
+        """call the reload method"""
+        self.reload()
---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- PREPARE A TEST MY SQL TABLE FOR THE PROJECT
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+GRANT USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
