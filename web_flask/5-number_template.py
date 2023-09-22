6fc9546596941b164c48014d53e6744e66073964
MohamedH1000 <mohammedhisham115@gmail.com>
2023-09-22 05:47:15 +0300

    render template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+a flask web application to be started by this script
+"""
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    the hello hbnb to be displayed
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """
+    same but display hbnb
+    """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """
+    display the value of tect after the vriable c
+    """
+    return "C {}".format(text.replace('_', ' '))
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text="is_cool"):
+    """
+    display the value is cool
+    that is default after the variable
+    python
+    """
+    return "Python {}".format(text.replace('_', ' '))
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    """
+    display " n is a number"
+    if n is integer
+    """
+    return "{} is a number".format(n)
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template_route(n):
+    """
+    other html page to be displayed
+    only and only if n is integer
+    """
+    return render_template('5-number.html', n=n)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
