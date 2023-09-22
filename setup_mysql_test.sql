bc2fb9ffafb1515e4b1340d3b1b82dc9739cfcb1
MohamedH1000 <mohammedhisham115@gmail.com>
2023-09-22 03:34:47 +0300

    test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,7 @@
 -- PREPARE A TEST MY SQL TABLE FOR THE PROJECT
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
