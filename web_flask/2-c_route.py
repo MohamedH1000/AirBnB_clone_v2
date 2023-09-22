2fe2215e138d07af0511f166826b5cbe9fd93b4d
MohamedH1000 <mohammedhisham115@gmail.com>
2023-09-22 05:29:33 +0300

    c variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+a flask web application to be started by this script
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    the hello hbnb to be displayed
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """
+    same but display hbnb
+    """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """
+    display the value of tect after the vriable c
+    """
+    return "C {}".format(text.replace('_', ' '))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
