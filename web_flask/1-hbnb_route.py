7494dcf9a663c02d468e5b4bd42af863c4e2279d
MohamedH1000 <mohammedhisham115@gmail.com>
2023-09-22 05:25:22 +0300

    adding url

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+a flask web application to be started by this script
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    the hello hbnb to be displayed
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """
+    same but display hbnb
+    """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
